b60a89496362d64c8240af8b4e056d38955b3ba9
Pedro Cruz <pedrovictor.lage@gmail.com>
2024-08-01 13:42:15 -0400

    Fix class initializer

--- go-saffe-swift/Classes/GoSaffeCapture.swift
+++ go-saffe-swift/Classes/GoSaffeCapture.swift
@@ -1,23 +1,69 @@
-import Foundation
+import UIKit
 import WebKit
 
-class GoSaffeCapture {
+class ViewController: UIViewController {
+
+    var webView: WKWebView?
     let captureKey: String
     let userIdentifier: String
     let type: String
     let endToEndId: String
-    var webView: WKWebView?
-
+    var isMessageReceived = false
+    
     init(captureKey: String, userIdentifier: String, type: String, endToEndId: String) {
         self.captureKey = captureKey
         self.userIdentifier = userIdentifier
         self.type = type
         self.endToEndId = endToEndId
+        super.init(nibName: nil, bundle: nil)
         self.webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
         self.webView?.navigationDelegate = self
     }
+    
+    required init?(coder: NSCoder) {
+        self.captureKey = ""
+        self.userIdentifier = ""
+        self.type = ""
+        self.endToEndId = ""
+        super.init(coder: coder)
+    }
+    
+    deinit {
+        print("LivenessViewController deinitialized")
+    }
 
-    func load() {
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        createWebView()
+        loadLiveness()
+    }
+
+    func createWebView() {
+        let webConfiguration = WKWebViewConfiguration()
+        let userContentController = WKUserContentController()
+        userContentController.add(self, name: "receiveMessage")
+        webConfiguration.allowsInlineMediaPlayback = true
+        webConfiguration.mediaTypesRequiringUserActionForPlayback = []
+        webConfiguration.userContentController = userContentController
+
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView?.navigationDelegate = self
+        view.addSubview(webView!)
+        webView?.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 11.0, *) {
+            let margins = self.view.safeAreaLayoutGuide
+            
+            NSLayoutConstraint.activate([
+                webView!.topAnchor.constraint(equalTo: margins.topAnchor),
+                webView!.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+                webView!.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+                webView!.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+            ])
+        }
+        
+    }
+
+    func loadLiveness() {
         let urlString = "https://go.saffe.ai/v0/capture"
         guard let url = URL(string: urlString) else {
             return
@@ -37,9 +83,11 @@ class GoSaffeCapture {
 
         webView?.load(request)
     }
+
 }
 
-extension GoSaffeCapture: WKNavigationDelegate {
+extension ViewController: WKNavigationDelegate {
+
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         let script = """
         (function() {
@@ -50,4 +98,37 @@ extension GoSaffeCapture: WKNavigationDelegate {
         """
         webView.evaluateJavaScript(script, completionHandler: nil)
     }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        // Falha na navegação
+    }
+
+}
+
+extension ViewController: WKScriptMessageHandler {
+
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if message.name == "receiveMessage" {
+            if let args = message.body as? [String: Any],
+               let source = args["source"] as? String, source == "go-saffe-capture",
+               let payload = args["payload"] as? [String: Any],
+               let event = payload["event"] as? String {
+
+                if event == "close" && !isMessageReceived {
+                    isMessageReceived = true
+                    webView = nil
+                    webView?.navigationDelegate = nil
+                    webView?.configuration.userContentController.removeScriptMessageHandler(forName: "receiveMessage")
+                    webView?.stopLoading()
+                } else if event == "finish" && !isMessageReceived {
+                    isMessageReceived = true
+                    webView = nil
+                    webView?.navigationDelegate = nil
+                    webView?.configuration.userContentController.removeScriptMessageHandler(forName: "receiveMessage")
+                    webView?.stopLoading()
+                }
+            }
+        }
+    }
+
 }
